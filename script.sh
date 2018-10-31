00f7c6c6cead78e5ff87ac30e96e757172f51c1a
Potapov <212717493@logon.ds.ge.com>
2018-10-31 16:47:03 +0100

    my

--- script.sh
+++ script.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 echo "Shell Start to generate package.xml ..."
-metaItems=(`git diff --name-status $GIT_PREVIOUS_COMMIT $GIT_COMMIT | xargs`)
-sleep 5s
+metaItems=(`git diff --name-status $GIT_PREVIOUS_BRANCH $GIT_NEXT_BRANCH | xargs`)
 echo "Shell: generate empty deployment folder: codeDeployPkg ..."
 pageString=""
 componentString=""
 clsString=""
 triggerString=""
-for i in ${!metaItems[@]}
+echo $metaItems
+sleep 3
+
+echo "now start"
+for i in ${!metaItems[@]} 
 do 
-    sleep 5s
     metaName=${metaItems[i]}
-    if [[ ${metaItems[i]} == *".page" ]] && [[ -f "$metaName" ]]; then
+    echo $metaName
+    sleep 2
+    if [[ ${metaItems[i]} == *".page" ]]; then
         pageName=${metaName#src/pages/}
         pageString=$pageString"<members>"${pageName%.page}"</members>"
         cp -p "$metaName" "codeDeployPkg/pages"
         cp -p "$metaName-meta.xml" "codeDeployPkg/pages"
-    elif [[ ${metaItems[i]} == *".component" ]] && [[ -f "$metaName" ]]; then
+    elif [[ ${metaItems[i]} == *".component" ]]; then
         componentName=${metaName#src/components/}
         componentString=$componentString"<members>"${componentName%.component}"</members>"
         cp -p "$metaName" "codeDeployPkg/components"
         cp -p "$metaName-meta.xml" "codeDeployPkg/components"
-    elif [[ ${metaItems[i]} == *".cls" ]] && [[ -f "$metaName" ]]; then 
+    elif [[ ${metaItems[i]} == *".cls" ]]; then 
+    	sleep 3;
+    	echo "Here"
+
         className=${metaName#src/classes/}
         clsString=$clsString"<members>"${className%.cls}"</members>"
         cp -p "$metaName" "codeDeployPkg/classes"
         cp -p "$metaName-meta.xml" "codeDeployPkg/classes"
-    elif [[ ${metaItems[i]} == *".trigger" ]] && [[ -f "$metaName" ]]; then
+    elif [[ ${metaItems[i]} == *".trigger" ]]; then
         triggerName=${metaName#src/triggers/}
         triggerString=$triggerString"<members>"${triggerName%.trigger}"</members>"
         cp -p "$metaName" "codeDeployPkg/triggers"
@@ -47,5 +54,4 @@ if [ "$triggerString" != "" ]; then
 fi
 packageString="<?xml version=\"1.0\" encoding=\"UTF-8\"?><Package xmlns=\"http://soap.sforce.com/2006/04/metadata\">$pageString$componentString$clsString$triggerString<version>36.0</version></Package>"
 echo $packageString > codeDeployPkg/package.xml
-sleep 5s
 echo "Shell: package.xml is ready!"
